57c3abeca12e982c76e962bcf870a8c86ebab37e
ДОМ <igor2870@qip.ru>
2016-05-14 23:52:11 +0300

    +if else

--- src/lesson_1/Exercise1.java
+++ src/lesson_1/Exercise1.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
  */
 public class Exercise1 {
     public static void main(String[] args) {
+       int b =20, c = 30;
         System.out.print("Exercise №1 - ");
         System.out.println("Scanner");
         Scanner sc = new Scanner(System.in);
         System.out.println("INPUT");
         int a = sc.nextInt();
         sc.close();
+        if(a<b){
+            System.out.println("little");
+        }else{
         System.out.println("a="+a);
-    }
+    }}}
 
-}
+
